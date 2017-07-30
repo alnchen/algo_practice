65cdaf5697c70f5c96ac19de4aeb4f46189bb5d7
Allen Chen <alchen1231@gmail.com>
2017-07-29 21:51:14 -0700

    more problems

--- algos.rb
+++ algos.rb
@@ -78,5 +78,15 @@ end
 
 
 # You are going to be given an array of integers. Your job is to take that array
-# and find an index N where the sum of the integers to the left of N is equal to the 
+# and find an index N where the sum of the integers to the left of N is equal to the
 # sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+def find_even_index(arr)
+  left = 0
+  right = arr.reduce(&:+)
+  arr.each_with_index do |x, i|
+    right -= x
+    return i if left == right
+    left += x
+  end
+  -1
+end
