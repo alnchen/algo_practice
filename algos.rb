95d8e7d617fa4cb8c64c8e9663fa16d892f66e28
Allen Chen <alchen1231@gmail.com>
2017-07-29 21:49:08 -0700

    more problems

--- algos.rb
+++ algos.rb
@@ -58,3 +58,11 @@ end
 # Write a function, persistence, that takes in a positive parameter num and returns
 # its multiplicative persistence, which is the number of times you must multiply the
 # digits in num until you reach a single digit.
+def persistence(n)
+  k = 0
+   while n > 9 do
+    n = n.to_s.split(//).map{|x| x.to_i}.inject(:*)
+    k+=1
+   end
+  k
+end
