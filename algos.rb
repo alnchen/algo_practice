9cff6258b55b8e7dc051f6ae23cdfbe88624b2ef
Allen Chen <alchen1231@gmail.com>
2017-07-29 21:45:30 -0700

    more problems

---
+++ algos.rb
@@ -0,0 +1,48 @@
+# The string "PAYPALISHIRING" is written in a zigzag pattern on a
+# given number of rows like this: (you may want to display this pattern
+# in a fixed font for better legibility)
+
+# And then read line by line: "PAHNAPLSIIGYIR"
+# Write the code that will take a string and make this conversion given a number of rows:
+
+def convert(s, num_rows)
+# naive approach
+  return s if num_rows == 1
+
+  output = Array.new(num_rows){ Array.new}
+  row = 0
+  down = true
+  s.length.times do |ltr_idx|
+      if down
+          output[row] << s[ltr_idx]
+          row += 1
+          if row == num_rows
+              row = num_rows - 2
+              down = false
+          end
+      else
+          output[row] << s[ltr_idx]
+          row -=1
+          if row == -1
+              down = true
+              row += 2
+          end
+      end
+  end
+  output.flatten.join('')
+end
+
+
+
+
+# Write a function to find the longest common prefix string amongst an array of strings.
+def longest_common_prefix(strs)
+    return '' if strs.empty?
+    word = strs.min_by {|word| word.length}
+    while word.length >= 1
+        end_idx = word.length - 1
+        return word if strs.all? {|others| others[0..end_idx] == word}
+        word.slice!(-1)
+    end
+    ""
+end
