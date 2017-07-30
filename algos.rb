d68d539eb2f6cb434d4fb4bb67e7a3d486ac60eb
Allen Chen <alchen1231@gmail.com>
2017-07-29 21:51:37 -0700

    more problems

--- algos.rb
+++ algos.rb
@@ -90,3 +90,10 @@ def find_even_index(arr)
   end
   -1
 end
+
+
+
+# You are given an array (which will have a length of at least 3, but could
+# be very large) containing integers. The array is either entirely comprised of 
+# odd integers or entirely comprised of even integers except for a single integer N.
+# Write a method that takes the array as an argument and returns N.
