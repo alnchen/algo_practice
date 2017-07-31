8f8efd43099a77a061b9505ccf6d1387f990bc52
Allen Chen <alchen1231@gmail.com>
2017-07-31 13:45:13 -0700

    added problems

--- algos.rb
+++ algos.rb
@@ -136,9 +136,15 @@ def valid_parenthesis(string)
   }
   string.chars.each do |char|
     if brackets[char]
-      stack 
+      stack
     else
 
     end
   end
 end
+
+
+# Given a string, you need to reverse the order of characters in each word within a
+# sentence while still preserving whitespace and initial word order.
+# Input: "Let's take LeetCode contest"
+# Output: "s'teL ekat edoCteeL tsetnoc"
