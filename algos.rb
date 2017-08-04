c09f3006569f34a3e4793bde04ef67f8f3611fa0
Allen Chen <alchen1231@gmail.com>
2017-08-03 19:35:47 -0700

    added problems

--- algos.rb
+++ algos.rb
@@ -185,26 +185,16 @@ end
 def min_deletions(str1, str2)
   a_hash = {}
   b_hash = {}
+  diff = 0
 
   str1.split('').each { |ltr| a_hash[ltr] ? a_hash[ltr] += 1 : a_hash[ltr] = 1}
   str2.split('').each { |ltr| b_hash[ltr] ? b_hash[ltr] += 1 : b_hash[ltr] = 1}
 
-  diff = 0
-  a_hash.each do |ltr, amt|
-      if b_hash[ltr]
-          diff += (amt - b_hash[ltr]).abs
-      else
-          diff += amt
-      end
-  end
+  a_hash.each { |ltr, amt| b_hash[ltr] ? diff += (amt - b_hash[ltr]).abs : diff += amt }
 
-  b_hash.each do |ltr, amt|
-      unless a_hash[ltr]
-          diff += amt
-      end
-  end
+  b_hash.each { |ltr, amt| diff += amt unless a_hash[ltr] }
 
   diff
 end
 
-p min_deletions('hello', 'lol')
+# p min_deletions('hello', 'lol')
