0b791c2010d5f8e17640b02d0e8bfbdf6d4d8ebc
Allen Chen <alchen1231@gmail.com>
2017-08-06 00:07:05 -0700

    added problems

--- algos.rb
+++ algos.rb
@@ -634,3 +634,13 @@ def arithmetic?(arr)
 
     true
 end
+
+
+
+# Given a sorted array consisting of only integers where every
+# element appears twice except for one element which appears once.
+# Find this single element that appears only once.
+# Input: [1,1,2,3,3,4,4,8,8]
+# Output: 2
+# Input: [3,3,7,7,10,11,11]
+# Output: 10
