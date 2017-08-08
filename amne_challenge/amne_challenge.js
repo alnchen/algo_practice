487ec46a9bec90a920149554607d1bcb65a2da5c
Allen Chen <alchen1231@gmail.com>
2017-08-07 17:52:13 -0700

    JS solution added

--- amne_challenge/amne_challenge.js
+++ amne_challenge/amne_challenge.js
@@ -1,21 +1,74 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-function subrangeDifference(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                console.log(allText);
-            }
-        }
-    }
-    rawFile.send();
+function subrangeDifference(file) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  let getData = rawFile.onreadystatechange = function () {
+      if(rawFile.readyState === 4) {
+          if(rawFile.status === 200 || rawFile.status === 0) {
+            var allText = rawFile.responseText;
+            return allText;
+          }
+      }
+  };
+  rawFile.send();
+  var parsedData = getData().split('\n');
+
+  var length = parseInt(parsedData[0].split(' ')[0]),
+    windowSize = parseInt(parsedData[0].split(' ')[1]),
+    priceData = parsedData[1].split(' ').map(num => parseInt(num));
+
+  var output = [];
+
+  for (var i = 0; i < (length - windowSize + 1); i++) {
+    let res = windowCalculation(priceData.slice(i, i + windowSize), windowSize);
+    console.log(res);
+    output.push(res);
+  }
+
+  return output;
 }
 
-subrangeDifference("file:///Users/alnchen/Desktop/aA/algos/amne_challenge/test1.txt");
+function windowCalculation(set, size) {
+  var res = 0;
+
+  for (var i = 0; i < size - 1; i++) {
+    for (var i2 = i + 1; i2 < size; i2++) {
+      var subRange = set.slice(i,i2 + 1);
+
+      if (increasing(subRange)) {
+        res += 1;
+      } else {
+        if (decreasing(subRange)) {
+          res -= 1;
+        }
+      }
+    }
+  }
+
+  return res;
+}
+
+function increasing(arr) {
+  for (var i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i+1]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function decreasing(arr) {
+  for (var i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < arr[i+1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let testFilePath1 = "file:///Users/alnchen/Desktop/aA/algos/amne_challenge/test1.txt"
+let testFilePath2 = "file:///Users/alnchen/Desktop/aA/algos/amne_challenge/test2.txt"
+
+subrangeDifference(testFilePath2);
